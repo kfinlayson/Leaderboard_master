83c5f77ae0680071692ee7f6ee18acff07ec83f1
ccook7 <ccook7@stu.jsu.edu>
2017-02-28 17:56:07 -0600

    GUI Clean-Up

--- src/main/java/edu/jsu/mcis/Gamegogy.java
+++ src/main/java/edu/jsu/mcis/Gamegogy.java
@@ -26,27 +26,35 @@ public class Gamegogy extends JPanel{
 		database = new Database();
 		courseComboBox = new JComboBox(database.getAllCourseIDsArray());
 		courseComboBox.setName("courseComboBox");
-		
+		courseComboBox.addItemListener(new ItemListener(){
+			public void itemStateChanged(ItemEvent item){
+				updateColumnComboBox();
+				updateLabels();
+			}
+		});
 		courseComboBox.setSelectedIndex(0);
-		Assignment assignment = new Assignment("99000");
 		
-		columnComboBox = new JComboBox(assignment.getAssignmentList());
+		columnComboBox = new JComboBox( new Assignment(database.getAllCourseIDsArray()[0]).getAssignmentList());
 		columnComboBox.setName("columnComboBox");
-		
 		columnComboBox.setSelectedIndex(0);
+		columnComboBox.addItemListener(new ItemListener(){
+			public void itemStateChanged(ItemEvent item){
+				updateLabels();
+			}
+		});
+		
 		
 		courseComboBox.setPreferredSize(new Dimension(100,65));
 		columnComboBox.setPreferredSize(new Dimension(100,65));
-		JLabel courseLabel = new JLabel("Course");
-		JLabel columnLabel = new JLabel("Column");
 		
 		JPanel dropDownGrid = new JPanel();
 		dropDownGrid.setLayout(new GridLayout(1,5));
-		dropDownGrid.add(courseLabel);
+		dropDownGrid.add(new JLabel("Course"));
 		dropDownGrid.add(courseComboBox);
 		dropDownGrid.add(new JLabel(""));
-		dropDownGrid.add(columnLabel);
+		dropDownGrid.add(new JLabel("Column"));
 		dropDownGrid.add(columnComboBox);
+		
 		
 		JPanel labelGridMain = new JPanel();
 		labelGridMain.setLayout(new GridLayout(1,3));
@@ -55,96 +63,81 @@ public class Gamegogy extends JPanel{
 		JPanel labelGrid2 = new JPanel();
 		labelGrid2.setLayout(new GridLayout(1,2));
 		
-		JLabel courseTermLabel = new JLabel("Term: ");
-		Course course = database.getCourse(courseComboBox.getSelectedItem().toString());
-		courseTerm = new JLabel(course.getCourseTerm() + " " + course.getCourseYear());
+		courseTerm = new JLabel();
 		courseTerm.setName("courseTerm");
-		labelGrid1.add(courseTermLabel);
+		labelGrid1.add(new JLabel("Term: "));
 		labelGrid1.add(courseTerm);
 		
-		JLabel courseEnrollmentLabel = new JLabel("Enrollment: ");
-		courseEnrollment = new JLabel(course.getCourseSize()); 
+		courseEnrollment = new JLabel(); 
 		courseEnrollment.setName("courseEnrollment");
-		labelGrid2.add(courseEnrollmentLabel);
+		labelGrid2.add(new JLabel("Enrollment: "));
 		labelGrid2.add(courseEnrollment);
 		
 		labelGridMain.add(labelGrid1);
 		labelGridMain.add(new JLabel(""));
 		labelGridMain.add(labelGrid2);
 		
+		
 		JPanel studentInfoBoxMain = new JPanel();
 		studentInfoBoxMain.setLayout(new GridLayout(1,2));
 		
 		JPanel studentInfoBox1 = new JPanel();
 		studentInfoBox1.setLayout(new GridLayout(4,1));
-		
-		JLabel studentIdLabel = new JLabel("ID: ");
-		studentInfoBox1.add(studentIdLabel);
-		
-		JLabel studentNameLabel = new JLabel("Name: ");
-		studentInfoBox1.add(studentNameLabel);
-		
-		JLabel studentEmailLabel = new JLabel("Email: ");
-		studentInfoBox1.add(studentEmailLabel);
-		
-		JLabel studentScoreLabel = new JLabel("Score: ");
-		studentInfoBox1.add(studentScoreLabel);
+		studentInfoBox1.add(new JLabel("ID: "));
+		studentInfoBox1.add(new JLabel("Name: "));
+		studentInfoBox1.add(new JLabel("Email: "));
+		studentInfoBox1.add(new JLabel("Score: "));
 		
 		JPanel studentInfoBox2 = new JPanel();
 		studentInfoBox2.setLayout(new GridLayout(4,1));
-		
-		
-		assignment.findMaxScore(columnComboBox.getSelectedItem().toString());
-		Student student = database.getStudent(assignment.getMaxScoreID());
-		studentId = new JLabel(student.getID());
+		studentId = new JLabel();
 		studentId.setName("studentId");
 		studentInfoBox2.add(studentId);
-		studentName = new JLabel(student.getFirstName() + " " + student.getLastName());
+		studentName = new JLabel();
 		studentName.setName("studentName");
 		studentInfoBox2.add(studentName);
-		studentEmail = new JLabel(student.getStudentEmail() + "@jsu.edu");
+		studentEmail = new JLabel();
 		studentEmail.setName("studentEmail");
 		studentInfoBox2.add(studentEmail);
-		studentScore = new JLabel(assignment.getMaxScore());
+		studentScore = new JLabel();
 		studentScore.setName("studentScore");
 		studentInfoBox2.add(studentScore);
 		
 		studentInfoBoxMain.add(studentInfoBox1);
 		studentInfoBoxMain.add(studentInfoBox2);
-		
 		studentInfoBoxMain.setBorder(BorderFactory.createLineBorder(Color.black));
+		
 		
 		setLayout(new GridLayout(3,1));
 		add(dropDownGrid);
 		add(labelGridMain);
 		add(studentInfoBoxMain);
 		
-		courseComboBox.addItemListener(new ItemListener(){
-			public void itemStateChanged(ItemEvent item){
-				String course = (String)courseComboBox.getSelectedItem();
-				Assignment temp = new Assignment(course);
-				DefaultComboBoxModel model = new DefaultComboBoxModel(temp.getAssignmentList());
-				columnComboBox.setModel(model);
-			}
-		});
-		columnComboBox.addItemListener(new ItemListener(){
-			public void itemStateChanged(ItemEvent item){
-				String course = (String)courseComboBox.getSelectedItem();
-				Assignment temp = new Assignment(course);
-				
-				String assignmentString = (String)columnComboBox.getSelectedItem();
-				courseTerm.setText(database.getCourse(course).getCourseTerm() + " " + database.getCourse(course).getCourseYear());
-				courseEnrollment.setText(database.getCourse(course).getCourseSize());
-				
-				temp.findMaxScore(assignmentString);
-				studentId.setText(temp.getMaxScoreID());
-				studentName.setText(database.getStudent(temp.getMaxScore()).getFirstName() + " " + database.getStudent(temp.getMaxScore()).getLastName()) ;
-				studentEmail.setText(database.getStudent(temp.getMaxScore()).getStudentEmail() + "@jsu.edu");
-				studentScore.setText(temp.getMaxScore());
-			}
-		});
+	
+		
 	}
 	
-	
+	private void updateColumnComboBox(){
+			String course = (String)courseComboBox.getSelectedItem();
+			Assignment temp = new Assignment(course);
+			DefaultComboBoxModel model = new DefaultComboBoxModel(temp.getAssignmentList());
+			columnComboBox = new JComboBox(model);
+			//columnComboBox.setSelectedIndex(0);
+	}
+	private void updateLabels(){
+			String course = (String)courseComboBox.getSelectedItem();
+			Assignment temp = new Assignment(course);
+				
+			String assignmentString = (String)columnComboBox.getSelectedItem();
+			courseTerm.setText(database.getCourse(course).getCourseTerm() + " " + database.getCourse(course).getCourseYear());
+			courseEnrollment.setText(database.getCourse(course).getCourseSize());
+				
+			temp.findMaxScore(assignmentString);
+			studentId.setText(temp.getMaxScoreID());
+			studentName.setText(database.getStudent(temp.getMaxScore()).getFirstName() + " " + database.getStudent(temp.getMaxScore()).getLastName()) ;
+			studentEmail.setText(database.getStudent(temp.getMaxScore()).getStudentEmail() + "@jsu.edu");
+			studentScore.setText(temp.getMaxScore());
+		
+	}
 	
 }
